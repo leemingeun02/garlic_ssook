2ec25b58fa9f4e00ac35220cf8ec034005a4deb4
leemingeun02 <164734663+leemingeun02@users.noreply.github.com>
2024-06-06 23:13:04 +0900

    online judge

--- programming_language/python/2204.py
+++ programming_language/python/2204.py
@@ -1,8 +1,20 @@
 N = int(input())
-words = []
 
-while N != 0:
-    for i in range(N):
-        words.append(input())
-    print(words)
-    N = int(input())
+answer = 0
+integers = list(map(int, input().split(" ")))
+
+
+def identify(a): ##소수인지 확인하는 함수 정의
+    flag = True
+    if a == 1:
+        flag = False
+    else:            
+        for i in range(2, a-1):
+            if a % i == 0:
+                flag *= 0
+                break
+    return flag
+for i in range(N):
+    if identify(integers[i]) == True:
+        answer += 1
+print(answer)
