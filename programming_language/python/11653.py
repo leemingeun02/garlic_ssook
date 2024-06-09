e16c782faa34d4bef7c30eeaf22d79eec648b6d8
leemingeun02 <164734663+leemingeun02@users.noreply.github.com>
2024-06-09 23:53:43 +0900

    oj

--- programming_language/python/11653.py
+++ programming_language/python/11653.py
@@ -3,9 +3,21 @@ input = sys.stdin.readline
 
 N = int(input())
 
+def identify(a): ##소수인지 확인하는 함수 정의
+    flag = 1
+    if a == 1:
+        flag = 0
+    else:            
+        for i in range(2, a-1):
+            if a % i == 0:
+                flag *= 0
+                break
+    return flag
+
 if N == 1:
     pass
-elif: #소수인 경우 즉 소인수분해가 불가능한 경우
+elif identify(N) == 1:
+    print(N) #소수인 경우 즉 소인수분해가 불가능한 경우 해당소수만 출력
 else: #소인수분해가 가능한경우
     for i in range(2, N):
         repeat = 0
