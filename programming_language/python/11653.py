a4d036474cb836556c0770f5acc7704b872cac2c
leemingeun02 <164734663+leemingeun02@users.noreply.github.com>
2024-06-09 23:48:31 +0900

    oj

--- programming_language/python/11653.py
+++ programming_language/python/11653.py
@@ -1,7 +1,17 @@
 import sys
-input = sys.stdin.readline().rstrip()
+input = sys.stdin.readline
 
 N = int(input())
 
-for i in range(N):
-    
+if N == 1:
+    pass
+elif: #소수인 경우 즉 소인수분해가 불가능한 경우
+else: #소인수분해가 가능한경우
+    for i in range(2, N):
+        repeat = 0
+        while repeat == 0:
+            if N % i == 0:
+                print(i)
+                N = N// i
+            else:
+                repeat = 1
