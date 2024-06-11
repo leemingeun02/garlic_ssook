43f4c001ee3430f24b95bce58e24b56b80754a80
leemingeun02 <164734663+leemingeun02@users.noreply.github.com>
2024-06-12 01:19:58 +0900

    boj

--- programming_language/python/1018.py
+++ programming_language/python/1018.py
@@ -1,1 +1,25 @@
-N, M = map(int, input().split(" "))
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+
+board = [0 for i in range(N)]
+for i in range(N):
+    board[i] = input()
+
+ideal_board_ver1 = ["WBWBWBWB", "BWBWBWBW"] * 4
+ideal_board_ver2 = ["BWBWBWBW", "WBWBWBWB"] * 4
+
+answer = 10000
+
+
+for i in range(N-7):
+    for j in range(M-7):
+        result1, result2 = 0, 0
+        for k in range(8):
+            for l in range(8):
+                if ideal_board_ver1[k][l] != board[i+k][j+l]:
+                    result1 += 1
+                if ideal_board_ver2[k][l] != board[i+k][j+l]:
+                    result2 += 1
+        answer = min(answer, result1, result2)
+print(answer)
--- programming_language/python/2231.py
+++ programming_language/python/2231.py
@@ -1,9 +1,12 @@
 N = input()
 
-for i in range(int(N)+1):
-    asdf = 0
+for i in range(int(N)):
     i = str(i)
-    for j in range(len(str(i))):
-        asdf += int(i) + int(i[j])
-    if asdf == N:
+    make_sum = int(i)
+    for j in range(len(i)):
+        make_sum += int(i[j])
+    if make_sum == int(N):
         print(i)
+        break
+    if int(i) == int(N) -1:
+        print(0)
