74d121d2ebd39e0bd2290054a40a257fd454577d
leemingeun02 <164734663+leemingeun02@users.noreply.github.com>
2024-06-13 23:43:41 +0900

    boj

--- programming_language/python/2839.py
+++ programming_language/python/2839.py
@@ -1,15 +1,12 @@
-N, M = map(int, input().split(" "))
-hangryeol = [0 for j in range(N)]
-hangryeol2 = [0 for j in range(N)]
-for i in range(N):
-    hangryeol[i] = list(map(int, input().split(" ")))
-for i in range(N):
-    hangryeol2[i] = list(map(int, input().split(" ")))
+N = int(input())
 
-hangryeol3 = [[0 for i in range(M)] for j in range(N)]
-for i in range(N):
-    for j in range(M):
-        hangryeol3[i][j] = hangryeol[i][j] + hangryeol2[i][j]
+result = []
+for i in range(N//3):
+    for j in range(N//5):
+        if 3*i + 5 * j == N:
+            result.append(i+j)
 
-for i in hangryeol3:
-    print(*i, sep=" ")
+if result:
+    print(min(result))
+else:
+    print(-1)
--- programming_language/python/tempCodeRunnerFile.py
+++ programming_language/python/tempCodeRunnerFile.py
@@ -1,8 +1,1 @@
-A, B, V = map(int, input().split(" "))
-
-answer = (V-B) // (A-B)
-
-print(answer)
-
-if (V - B) - answer*(A-B) - A <= 0:
-    answer += 1
+print(min(result1, result2, result3))
